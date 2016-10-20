6470abd93cfa5c65b9726778d744e7547604465d
somename <someemail>
2016-10-20 14:37:56 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,4 +8,10 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void test_new_planet_x(){
+		PlanetExplorer PlanetExplorer = new PlanetExplorer(100,100,"");
+		assertEquals("",100,PlanetExplorer.x);
+	}
 }
