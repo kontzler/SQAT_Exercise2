8ba3d90cba736a88fa28d1991e2e6dfb8f6412c2
somename <someemail>
2016-10-20 16:23:06 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -77,7 +77,7 @@ public class PlanetExplorer {
 	public boolean isOnObstacle(){
 		int i=0;
 		String[] Obstacles = this.obstacle.split(" ");
-		System.out.println(Obstacles[0]);
+		if(Obstacles[0]==executeCommand("pos"))return true;
 		for(i=0;i<Obstacles.length;i++){
 			if(Obstacles[i]==executeCommand("pos"))return true;
 		}
