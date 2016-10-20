f36009be4d342c819ffe68df98418c06bad5b885
somename <someemail>
2016-10-20 16:03:11 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -68,7 +68,7 @@ public class PlanetExplorer {
 		if(pos_y==-1)pos_y=99;
 		//if(isOnObstacle()==true){
 			 //for(i=0;i<Obs_located.size();i++){
-				 if(Obs_located.get(i)==executeCommand("pos"))occ++;
+				// if(Obs_located.get(i)==executeCommand("pos"))occ++;
 			 //}
 			 //if(occ==0)Obs_located.add(executeCommand("pos"));
 		//}
