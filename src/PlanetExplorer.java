4668e764e3937e98882babb9a6db670f6a10e078
somename <someemail>
2016-10-20 15:05:05 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -55,6 +55,14 @@ public class PlanetExplorer {
 			if(orientation=="W"){pos_x--; return null;}
 		}
 		
+		if(command=="b"){
+			if(orientation=="N"){pos_y--;return null;}
+			if (orientation=="E"){pos_x--; return null;}
+			if(orientation=="S"){pos_y++; return null;}
+			if(orientation=="W"){pos_x++; return null;}
+			
+		}
+		
 		if(command=="")return "("+this.pos_x+","+this.pos_y+","+orientation+")";
 		
 		return "("+this.pos_x+","+this.pos_y+","+orientation+")";
